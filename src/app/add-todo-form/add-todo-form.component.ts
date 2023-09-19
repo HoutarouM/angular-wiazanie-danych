e2d123727de6766c6a3648fdcedc3d3874f970b3
Marian Horoshko <marian.horoshko@gmail.com>
2023-09-19 18:20:11 +0200

    connect add todo form to service

--- src/app/add-todo-form/add-todo-form.component.ts
+++ src/app/add-todo-form/add-todo-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Todo } from '../todo-list/todo.interface';
+import { TodoService } from '../todo-list/todo.service';
 
 @Component({
   selector: 'app-add-todo-form',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-todo-form.component.css'],
 })
 export class AddTodoFormComponent {
+  counter: number = 1;
   todoText: string = '';
+
+  constructor(private data: TodoService) {}
+
+  onSubmit(): void {
+    const todo: Todo = {
+      id: this.counter,
+      todo: this.todoText,
+      created: new Date(),
+      isDone: false,
+    };
+
+    this.data.addTodo(todo);
+
+    this.counter++;
+  }
 }
