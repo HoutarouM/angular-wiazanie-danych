52af90aa673907c7620af22d29031a027f406f55
Marian Horoshko <marian.horoshko@gmail.com>
2023-09-19 18:20:54 +0200

    listen todo service in todo list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Todo } from './todo.interface';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
 
+  constructor(private data: TodoService) {}
+
+  ngOnInit(): void {
+    this.data.todoSource.subscribe((todos) => (this.todos = todos));
+  }
 }
